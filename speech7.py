ae1686e8aa75bf044bf65c905f88aef25d2a677a
zeyneperismiss <61026249+zeyneperismiss@users.noreply.github.com>
2020-02-15 19:09:40 +0000

    Update and rename speech6.py to speech7.py

--- speech7.py
+++ speech7.py
@@ -17,7 +17,6 @@ with sr.Microphone() as source:
         
     if text == 'activate voice control':
         
-        with sr.Microphone() as source:
             #r.adjust_for_ambient_noise(source, duration = 2) #records background noise for 1 second and adjusts to this noise level
             print('Speak Anything: ')
             audio = r.record(source,duration=10)
@@ -30,12 +29,12 @@ with sr.Microphone() as source:
 
         #Robot gives reply in response to certain phrases or commands
                 
-        if text == 'hello':
-            reply = 'hello to you too'
-            print(reply)
+            if text == 'hello':
+                reply = 'hello to you too'
+                print(reply)
             
-        if text == 'grab the TV remote':
-            reply = 'Ok, fetching the TV remote'
-            print(reply)
+            if text == 'grab the TV remote':
+                reply = 'Ok, fetching the TV remote'
+                print(reply)
 
             
